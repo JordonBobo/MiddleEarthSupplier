03113064962a9f58d9cefee6af9cda5caa8f33c6
Dameon Davis <dad1977ut@yahoo.com>
2021-03-24 22:15:31 -0600

    add post function

---
+++ public/js/staff.js
@@ -0,0 +1,38 @@
+ $(document).ready(() => {
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+const product =$('#productName')
+const description =$('#productDescription')
+const image =$('#productImage')
+const price =$('#productPrice')
+const button =$('#newProductForm')
+button.on('submit',event=>{
+    event.preventDefault();
+    const productData = {
+        product: product.val().trim(),
+        description: description.val().trim(),
+        image: image.val().trim(),
+        price: price.val().trim(),
+    }
+    if(!productData.product || !productData.description || !productData.image || productData.price){
+        return;
+    }
+    addProduct(productData)
+    product.val('');
+    description.val('');
+    image.val('');
+    price.val('');
+})
+function addProduct(productData) {
+  $.post('/api/products',productData).then(console.log(productData))
+  .catch(err => {
+      console.log(err)
+  })
+}
+
+
+
+  $.get("/api/user_data").then(data => {
+    $(".member-name").text(data.email);
+  });
+}); 
